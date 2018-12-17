59293de66021fe062ddc8402ce715c140c400b4f
TuanAnhPham <ptuananh196@gmail.com>
2018-12-18 00:57:32 +0700

    .

--- src/base/enemysummoner/EnemySummonerPart.java
+++ src/base/enemysummoner/EnemySummonerPart.java
@@ -1,7 +1,9 @@
 package base.enemysummoner;
 
+import base.FrameCounter;
 import base.GameObject;
 import base.action.*;
+import base.enemy.StraightBullet;
 import base.physics.BoxCollider;
 import base.physics.Physics;
 import base.renderer.BoxRenderer;
@@ -11,6 +13,7 @@ import java.awt.*;
 public class EnemySummonerPart extends GameObject implements Physics {
     BoxCollider boxCollider;
     Action action;
+    FrameCounter summonStraightBullet = new FrameCounter(80);
 
     public EnemySummonerPart() {
         this.boxCollider = new BoxCollider(this.anchor,this.position
@@ -18,15 +21,21 @@ public class EnemySummonerPart extends GameObject implements Physics {
         this.renderer = new BoxRenderer(this.boxCollider
                 , Color.WHITE, true);
         this.position.set(0, 0);
-
     }
 
+    void createStraightBullet() {
+        if (this.summonStraightBullet.run()) {
+            StraightBullet straightBullet = GameObject.recycle((StraightBullet.class));
+            straightBullet.position.set(this.position.add(50, 0));
+            this.summonStraightBullet.reset();
+        }
+    }
 
 
     @Override
     public void run() {
         super.run();
-
+        this.createStraightBullet();
     }
 
     @Override
